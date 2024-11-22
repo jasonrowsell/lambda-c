f0fdbe58cf79d8a9fe468732bdffe617e9a3a910
jasonrowsell <jasonrowsell@hotmail.co.uk>
2024-11-22 17:37:40 +0000

    Add typedef

--- lambda.c
+++ lambda.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+// Macro to define a lambda function.
+// R  - Return type
+// params - List of function parameters
+// body - Function body
 #define LAMBDA(R, params, body) \
     ({ R __lambda__ params body __lambda__; })
 
+typedef int (*add_fn)(int, int);
+
 int main()
 {
-    int (*add)(int, int) = LAMBDA(int, (int a, int b), { return a + b; });
+    add_fn add = LAMBDA(int, (int a, int b), {
+        return a + b;
+    });
 
     int result = add(3, 4);
     printf("Result: %d\n", result);
