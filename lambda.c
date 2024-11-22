414516988b15a7924ff5efc721c43fbfd15ee62c
jasonrowsell <jasonrowsell@hotmail.co.uk>
2024-11-22 11:55:14 +0000

    Use line num for uniq fn name

--- lambda.c
+++ lambda.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define fn "abc"
+#define LAMBDA(arg, body) \
+    int lmb__LINE__(int arg) { return body; }
 
-int main() {
-    printf("%s\n", fn);
+void abc() {}
+
+int main()
+{
+    LAMBDA(x, x + 1)
 
     return 0;
 }
